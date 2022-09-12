ee4fdee2639db1a9da9e2fa17a229fcd71f4a154
Boby Darmawan <2011522023@student.unand.ac.id>
2022-09-12 21:04:57 +0700

    add change

--- app/src/main/java/com/example/kadep_kelompok2/MainActivity.java
+++ app/src/main/java/com/example/kadep_kelompok2/MainActivity.java
@@ -11,4 +11,8 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
     }
+
+    protected void tes(Bundle savedInstanceState) {
+
+    }
 }
